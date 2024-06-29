d25bc1f7b82ef3db6c4aa00e9e8a02a5d2dbc7a2
Emil Kelhälä <emil.kelhala@gmail.com>
2024-06-29 12:56:21 +0300

    Add commons-cli for command-line options

--- src/main/java/com/emilkelhala/Main.java
+++ src/main/java/com/emilkelhala/Main.java
@@ -1,9 +1,24 @@
 package com.emilkelhala;
 
-import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.commons.cli.ParseException;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Options;
 
 public class Main {
     public static void main(String[] args) {
-        
+        final Options options = new Options();
+        options.addRequiredOption("f", "files", true, "PDF files to merge");
+        options.addRequiredOption("o", "output", true, "Path to the output file");
+        CommandLineParser parser = new DefaultParser();
+        try {
+            CommandLine commandLine = parser.parse(options, args);
+
+        }
+        catch(ParseException exception) {
+            System.out.println(exception.getMessage());
+        }
     }
 }
