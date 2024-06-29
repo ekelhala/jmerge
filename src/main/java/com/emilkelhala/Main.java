e7e6fb39de700570180671b58cf8b3687be0bac6
Emil Kelhälä <emil.kelhala@gmail.com>
2024-06-29 12:38:01 +0300

    Initial commit

---
+++ src/main/java/com/emilkelhala/Main.java
@@ -0,0 +1,9 @@
+package com.emilkelhala;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ src/main/java/com/emilkelhala/PDFMerger.java
@@ -0,0 +1,39 @@
+package com.emilkelhala;
+
+import java.io.IOException;
+
+import org.apache.pdfbox.multipdf.PDFMergerUtility;
+import org.apache.pdfbox.pdmodel.PDDocument;
+
+public class PDFMerger {
+    
+    private PDFMergerUtility merger;
+
+    /**
+     * Setup the merger
+     * @param dest The destination file to be written
+     */
+    public PDFMerger(String dest){
+        merger = new PDFMergerUtility();
+        merger.setDestinationFileName(dest);
+    }
+
+    /**
+     * 
+     * Merge documents to each other and return the result
+     * 
+     * @param documents Documents to be merged
+     * @return Document containing the merged documents, in the order they were given
+     */
+    public PDDocument merge(PDDocument[] documents) throws IOException {
+        PDDocument ret = new PDDocument();
+        for(PDDocument document : documents) {
+                merger.appendDocument(ret, document);
+        }
+        return ret;
+    }
+
+    public void save() throws IOException {
+        merger.mergeDocuments(null);
+    }
+}
